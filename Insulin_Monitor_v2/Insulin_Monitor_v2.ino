b8d28a4dcfed3303d418074e093e104465a8fb6a
Linus Dillon <ldillon@Linuss-MacBook-Air.local>
2014-12-11 22:01:31 +1000

    Baseline.

---
+++ Insulin_Monitor_v2/Insulin_Monitor_v2.ino
@@ -0,0 +1,205 @@
+/*********************************************************************
+Insulin Monitor v2
+* OLED 128x32 I2C SSD1306 Display
+* DS18B20 Temperature Sensor
+* 5-Way Navigation Stick
+* Adafruit 3.3V 12MHz Trinket Pro
+*********************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <OneWire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
+
+// Display
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+// Temperature Sensor
+#define TEMP_SENSOR_PIN 3
+
+// Navigation Stick
+#define NAV_N_PIN 8
+#define NAV_S_PIN 10
+#define NAV_E_PIN 11
+#define NAV_W_PIN 12
+#define NAV_C_PIN 9
+
+int loopCounter;
+
+void setup()   {                
+  // Setup the pins for the navigation stick
+  pinMode(NAV_N_PIN, INPUT_PULLUP);
+  pinMode(NAV_S_PIN, INPUT_PULLUP);
+  pinMode(NAV_E_PIN, INPUT_PULLUP);
+  pinMode(NAV_W_PIN, INPUT_PULLUP);
+  pinMode(NAV_C_PIN, INPUT_PULLUP);
+  
+  pinMode(13, OUTPUT);
+  loopCounter = 0;
+
+  // Initialise the temperature sensor
+  getTemperature2(TEMP_SENSOR_PIN, 1);
+
+  // Initialise the OLED display
+  // generate the high voltage from the 3.3v line internally
+  // initialize with the I2C addr 0x3C (for the 128x32)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  
+  display.display();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);  
+  
+  // Setup interrupts for navigation stick
+  pciSetup(NAV_N_PIN);
+  pciSetup(NAV_S_PIN);
+  pciSetup(NAV_E_PIN);
+  pciSetup(NAV_W_PIN);
+  pciSetup(NAV_C_PIN);
+  
+  // Setup the watch dog timer
+  wdtSetup();
+}
+
+
+void loop() {
+  int temperature = getTemperature2(TEMP_SENSOR_PIN, 0);
+
+  displayOn();
+  display.clearDisplay();
+  //drawTemperatureHistory();
+  display.setCursor(8, 10);
+  display.print(temperature);
+  
+  if (digitalRead(NAV_N_PIN) == LOW) display.print("N");
+  if (digitalRead(NAV_S_PIN) == LOW) display.print("S");
+  if (digitalRead(NAV_E_PIN) == LOW) display.print("E");
+  if (digitalRead(NAV_W_PIN) == LOW) display.print("W");
+  if (digitalRead(NAV_C_PIN) == LOW) display.print("C");
+  
+  loopCounter ++;
+  display.print(loopCounter);
+  
+  display.display();
+  
+  delay(2000);
+  
+  displayOff();
+  
+  sleep();
+}
+
+void displayOn() {
+  display.ssd1306_command(SSD1306_DISPLAYON);
+}
+
+void displayOff() {
+  display.ssd1306_command(SSD1306_DISPLAYOFF);
+}
+
+void drawTemperatureHistory() {
+  display.setCursor(0, 0);
+  display.println("-1d");
+
+  display.setCursor(79, 0);
+  display.println("Now");
+  
+  display.drawRect(0, 8, 96, 24, WHITE);
+}
+
+// Sets up the pin change interrupt on the given pin
+void pciSetup(byte pin)
+{
+    *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+
+void wdtSetup() {
+  // Clear the reset flag
+  MCUSR &= ~(1<<WDRF);
+  
+  // In order to change WDE or the prescaler, we need to set WDCE (This will allow updates for 4 clock cycles)
+  WDTCSR |= (1<<WDCE) | (1<<WDE);
+
+  // Set new watchdog timeout prescaler value
+  WDTCSR = 1<<WDP0 | 1<<WDP3; /* 8.0 seconds */
+  
+  // Enable the WD interrupt (note no reset)
+  WDTCSR |= _BV(WDIE);
+}
+
+// Enters sleep mode to be woken on a pin change or the watchdog timer after 8 seconds
+// Various sources went into this, such as:
+// * http://donalmorrissey.blogspot.com.au/2010/04/putting-arduino-diecimila-to-sleep-part.html
+void sleep() {
+    // Attach to interrupt 0 (pin change on port B and external interrupt 0)
+    
+    // Choose our preferred sleep mode
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+ 
+    // Set sleep enable (SE) bit
+    sleep_enable();
+ 
+    // Put the device to sleep
+    sleep_mode();
+ 
+    // Upon waking up, sketch continues from this point
+    sleep_disable();
+    
+    // Re-endable all this peripherals
+    power_all_enable();
+}
+
+// Interupt handler for pin change interrupts on port B
+ISR(PCINT0_vect) {
+   // This is called when the interrupt occurs, but it doesn't need to do anything as the
+   // interrupt is just used to wake the microcontroller from sleep
+   
+  if (digitalRead (13) == HIGH)
+    digitalWrite (13, LOW);
+  else
+    digitalWrite (13, HIGH);
+}
+
+// Interupt handler forwatchdog timer
+ISR(WDT_vect) {
+   // This is called when the interrupt occurs, but it doesn't need to do anything as the
+   // interrupt is just used to wake the microcontroller from sleep
+}
+
+// Code taken from http://www.scargill.net/reading-dallas-ds18b20-chips-quickly/
+// Thanks to Peter Scargill
+int16_t getTemperature2(int x, byte start)
+{
+  OneWire ds(x);
+  byte i;
+  byte data[2];
+  int16_t result;
+  
+  do
+  {
+    ds.reset();
+    ds.write(0xCC);  // skip command
+    ds.write(0xBE);  // read 1st 2 bytes of scratchpad
+    for (i = 0; i < 2; i ++) data[i] = ds.read();
+    result = (data[1] << 8) | data[0];
+    result >>= 4;
+    if (data[1] & 128) result |= 61440;
+    if (data[0] & 8) ++result;
+    ds.reset();
+    ds.write(0xCC); // skip command
+    ds.write(0x44, 1);  // start conversion, assuming 5V connected
+    if (start) delay(1000);
+  }
+  while (start --);
+  return result;
+}
+
